7af824ecaa3f46728595bc4f8542658c31b63fb7
hansori01 <19sorihan88@gmail.com>
2021-02-24 04:36:23 +0000

    trying to add resizing using jquery on text input size

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
   $("textarea").on('input', function () {
     let charCount = 140 - this.value.length;
     $("output").text(charCount);
-    
+
     if (charCount < 21) {
       $("output").css('color', 'LightSalmon');
     } if (charCount < 11) {
@@ -15,6 +15,16 @@ $(document).ready(function () {
       $("output").css('color', '');
     }
   })
+
+  // $("textarea")
+  // .each(function () {
+  //   this.setAttribute("style", `${this.scrollHeight}px; overflow-y:hidden;`);
+  // })
+  // .on("input", function () {
+  //   this.style.height = "auto";
+  //   this.style.height = `${this.scrollHeight}px`;
+  // });
+
 });
 
 //JQuery feature: console.log(this) logs the current html element.
