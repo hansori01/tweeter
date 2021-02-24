8c06db04c9ba6a6c9854cf507f4509a2e0b5bb3e
hansori01 <19sorihan88@gmail.com>
2021-02-24 20:11:25 +0000

    adding client.js function of adding db tweets. additional html and css adjustments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,58 +1,55 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function () {
+  /*
+   * Client-side JS logic goes here
+   * jQuery is already loaded
+   * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+   */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
+  // Fake data taken from initial-tweets.json
+  const data = {
     "user": {
       "name": "Newton",
-      "avatars": "../images/profile2.png",
+      "avatars": "/images/profile2.png",
       "handle": "@SirIsaac"
     },
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "../images/profile4.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
   }
-]
 
-// const renderTweets = function(tweets) {
-// // loops through tweets
-// // calls createTweetElement for each tweet
-// // takes return value and appends it to the tweets container
-// }
-
-const createTweetElement = function(tweet) {
-  //takes in a tweet object
-let $tweet = $(`
+  // const renderTweets = function(tweets) {
+  // // loops through tweets
+  // // calls createTweetElement for each tweet
+  // // takes return value and appends it to the tweets container
+  // }
 
 
 
+  const createTweetElement = function (tweet) {
+    //takes in a tweet object
+    let $tweet = $(`<article>
+    <header>
+    <div class='tweeter'>
+    <span class ='profile'>
+    <img src=${tweet.user.avatars}> 
+    ${tweet.user.name}
+    </span>
+    <span class='handle'>${tweet.user.handle}</span>
+    </div>
+    </header>
+    <div class='tweet'>${tweet.content.text}<hr></div>
+    <footer>
+    <div class ='timestamp'><p>1s ago</p></div>
+    <ul class='interact' style="list-style-type:none">
+    <li id='share'>↩</li>
+    <li id='flag'>☆</li>
+    <li id='heart'>❤</li>
+    </ul>       
+    </footer>
+    </article>
+    `)
 
-
-`)
-//returns a tweet <article> containing the HTML structure of tweet
-
-
-
-
-return $tweet;
-}
-
-createTweetElement(data)
-
-
-// renderTweets(data);
+    return $tweet;
+  }
+});
