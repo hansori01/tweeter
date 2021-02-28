ba398992c70a441cdcf54a568ce3ded7789172e1
hansori01 <19sorihan88@gmail.com>
2021-02-28 21:00:48 +0000

    refactoring logic to helper function file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,18 @@
 $(document).ready(function () {
 
+
   //Counts character and changes color
   $('textarea').on('input', charCountColour);
 
   // loops array of tweet objects and appends each to ".messages"
   const renderTweets = function (tweets) {
-    //clears pre-loaded tweets to avoid loading every submit. Better way to do this?
+    //if u can get away with it, empty-> repopulate works
     $('.messages').empty();
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('.messages').prepend($tweet);
     }
   };
-
-  // individual Tweet HTML body is returned
-  const createTweetElement = function (tweet) {
-
-    //moment to update timestamp
-    const timeStamp = moment(tweet.created_at).fromNow();
-    const $tweet = $(`<article></article>`);
-    const header = $(
-      `<header>
-  <div class='tweeter'>
-  <span class ='profile'>
-  <img src=${tweet.user.avatars}> 
-  ${tweet.user.name}
-  </span>
-  <span class='handle'>${tweet.user.handle}</span>
-  </div>
-  </header>`
-    );
-    const safeInput = $("<div>").text(tweet.content.text);
-    safeInput.addClass('tweet');
-    const footer = $(
-      `<footer>
-  <div class ='timestamp'><p>${timeStamp}</p></div>
-  <div class='interact' style="list-style-type:none">
-  <i class="far fa-flag "></i>
-  <i class="fas fa-retweet"></i>
-  <i class="far fa-heart"></i>
-  </div>       
-  </footer>`
-    );
-
-    $tweet
-      .append(header)
-      .append(safeInput)
-      .append(footer);
-
-    return $tweet;
-  };
-
 
   // user submits a tweet
   $('form').submit(function (event) {
@@ -59,35 +21,18 @@ $(document).ready(function () {
     const tweetLength = $('textarea').val().length;
 
     //alerts thrown if length is 0 or too long
-    if (tweetLength > 140) {
-      $('#compose').fadeToggle(0);
-      $('#me').fadeToggle(0);
-      $('#long').fadeToggle(0);
-      $('#error').fadeToggle(0);
-    } else if (tweetLength === 0) {
-      $('#compose').fadeToggle(0);
-      $('#me').fadeToggle(0);
-      $('#short').fadeToggle(0);
-      $('#error').fadeToggle(0);
-    } else {
+    if (validateTweet(tweetLength)) {
       $.ajax({
         url: '/tweets',
         method: "POST",
         data: tweetData
       })
         .then(loadTweets)
-        .then($('#compose').show(300))
-        .then($('#me').show(300))
-        .then($('#long').hide(0))
-        .then($('#short').hide(0))
-        .then($('#error').hide(0))
-        .then($('textarea').val(''))
-        .then($('output').text('140'))
+        .then(resetState)
         .catch(err => console.log(err));
-    }
+    };
   });
 
-  // Fetch tweets to display on page
   const loadTweets = function () {
     return $
       .ajax({
@@ -100,7 +45,7 @@ $(document).ready(function () {
   };
   loadTweets();
 
-  // Nav bar text to text area
+  // Nav bar click to focus on text area
   $('#navCompose')
     .css('cursor', 'pointer')
     .click(function () {
--- public/scripts/clientHelper.js
+++ public/scripts/clientHelper.js
@@ -1,18 +1,80 @@
 
 // Counts characters left and changes colour
-const charCountColour = function() {
+const charCountColour = function () {
 
-  let charCount = 140 - this.value.length;
-  $('output').text(charCount);
+   const charCount = 140 - this.value.length;
+   $('output').text(charCount);
 
-  if (charCount < 21) {
-     $('output').css('color', 'LightSalmon');
-  } if (charCount < 11) {
-     $('output').css('color', 'Tomato');
-  } if (charCount < 1) {
-     $('output').css('color', '#8B0000');
-  }
-  if (charCount >= 21) {
-     $('output').css('color', '');
-  }
+   if (charCount < 21) {
+      $('output').css('color', 'LightSalmon');
+   } if (charCount < 11) {
+      $('output').css('color', 'Tomato');
+   } if (charCount < 1) {
+      $('output').css('color', '#8B0000');
+   }
+   if (charCount >= 21) {
+      $('output').css('color', '');
+   }
 };
+
+
+const validateTweet = tweetLength => {
+   const tooLong = tweetLength > 140;
+   const isEmpty = tweetLength === 0;
+
+   if (tooLong || isEmpty) {
+      const errorMessage = tooLong ? '#long' : '#short';
+      $('#compose').fadeToggle(0);
+      $('#me').fadeToggle(0);
+      $(errorMessage).fadeToggle(0);
+      $('#error').fadeToggle(0);
+      return false;
+   }
+   return true;
+}
+
+const resetState = () => {
+   $('#compose').show(300);
+   $('#me').show(300);
+   $('#long').hide(0);
+   $('#short').hide(0);
+   $('#error').hide(0);
+   $('textarea').val('');
+   $('output').text('140');
+}
+
+const createTweetElement = function (tweet) {
+       //using 'moment.js' to update timestamp
+       const timeStamp = moment(tweet.created_at).fromNow();
+       const $tweet = $(`<article></article>`);
+       const header = $(
+         `<header>
+     <div class='tweeter'>
+     <span class ='profile'>
+     <img src=${tweet.user.avatars}> 
+     ${tweet.user.name}
+     </span>
+     <span class='handle'>${tweet.user.handle}</span>
+     </div>
+     </header>`
+       );
+       const safeInput = $("<div>").text(tweet.content.text);
+       safeInput.addClass('tweet');
+       const footer = $(
+         `<footer>
+     <div class ='timestamp'><p>${timeStamp}</p></div>
+     <div class='interact' style="list-style-type:none">
+     <i class="far fa-flag "></i>
+     <i class="fas fa-retweet"></i>
+     <i class="far fa-heart"></i>
+     </div>       
+     </footer>`
+       );
+   
+       $tweet
+         .append(header)
+         .append(safeInput)
+         .append(footer);
+   
+       return $tweet;
+     };
